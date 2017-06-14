78932a2589444616c403099159fd793068cbbbc3
KevinLongCheng <myth282429096@163.com>
2017-06-14 21:15:22 +0800

    Snake code
    Snake move

---
+++ Snake/Snake/Assets/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+//	初始化定时器
+	private float Timer;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Timer += Time.deltaTime;
+		if (Timer > 0.5f) {
+			transform.position += new Vector3 (0f, 0f, 1f);	
+			Timer = 0f;
+		}
+	}
+}
